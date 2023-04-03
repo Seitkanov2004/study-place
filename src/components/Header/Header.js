826468c89b063e07a131594879620dab73dc56d7
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-04-03 14:39:41 -0700

    don't error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,18 +11,42 @@ const Header = () => {
     return (
         <div id='header'>
             <div className='container'>
-                <div className='header'>
-                    <div className='header--logo'>
-                        <img src={Logo} alt='logo' />
-                        <span>SELF DEVELOPING SCHOOL</span>
+                <div className="header--child">
+                    <div className='header'>
+                        <div className='header--logo'>
+                            <img src={Logo} alt='logo' style={{
+                                width: menu ? "30%" : "88%"
+                            }} />
+                            <span style={{
+                                display: menu ? "block" : "none"
+                            }}>SELF DEVELOPING SCHOOL</span>
+                        </div>
+                        <nav className='header--navbar'>
+                            <NavLink to={"/"} >Главная</NavLink>
+                            <NavLink to={"/our-cours"} >Наши курсы</NavLink>
+                            <NavLink to={"/about-us"} >О нас</NavLink>
+                        </nav>
+                        <div className='header--login'>
+                            <button className='header--login__btn' >
+                                <CiUser className='header--login__btn--icon' />Войти</button>
+                        </div>
+                        <div onClick={() => setMenu(!menu)} className='header--menu'>
+                            <div className='header--menu__parentMenu'>
+                                <div className={menu ? "header--menu__parentMenu--one" : ""} />
+                                <div className={menu ? "header--menu__parentMenu--tow" : ""} />
+                                <div className={menu ? "header--menu__parentMenu--three" : ""} />
+                            </div>
+                        </div>
                     </div>
-                    <nav className='header--navbar'>
-                        <NavLink to={"/"} >Главная</NavLink>
-                        <NavLink to={"/"} >Наши курсы</NavLink>
-                        <NavLink to={"/"} >О нас</NavLink>
-                    </nav>
-                    <div className='header--login'>
-                        <button className='header--login__btn' > <CiUser className='header--login__btn--icon' />Войти</button>
+                    <div style={{
+                        display: menu? "block" : "none",
+                    }}>
+                        <div className="header--nav" >
+                            <NavLink to={"/"} >Главная</NavLink>
+                            <NavLink to={"/"} >Наши курсы</NavLink>
+                            <NavLink to={"/"} >О нас</NavLink>
+                            <button className='header--towBtn'><CiUser className='header--towBtn__icon' />Войти</button>
+                        </div>
                     </div>
                     <div className='header--menu'>
                         <div/>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Follow from "./Follow/Follow";
+import Hero from "./Hero/Hero"
 
 const Home = () => {
     return (
         <div>
+            <Hero/>
             <Follow/>
-
+            
         </div>
     );
 };
