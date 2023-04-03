78d7374ad3dd47a082887684f6c6f4f4889a6f80
m0khina <mohina.artikova.kk@gmail.com>
2023-04-03 13:34:07 +0300

    add the follow section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Follow from "./Follow/Follow";
 
 const Home = () => {
     return (
         <div>
-            
+            <Follow/>
+
         </div>
     );
 };
