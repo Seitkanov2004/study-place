ad717638a7dcc8e6bb271aec20df7dc368685d6b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-04-03 17:31:12 -0700

    fifshed footer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Follow from "./Follow/Follow";
 import Hero from "./Hero/Hero"
+import Staty from './Staty/Staty';
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <Staty/>
             <Follow/>
-            
         </div>
     );
 };
