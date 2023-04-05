ef75eef9113b330e5ba3bc29d0467b62f9d1ff2a
ramazan784dev <rsaparbekov784@gmail.com>
2023-04-05 07:28:58 +0600

    I did accordion

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from "./Hero/Hero";
+import Accardeon from "./Accardeon/Accardeon";
 
 const Home = () => {
     return (
         <div>
            <Hero/>
+            <Accardeon/>
         </div>
     );
 };
